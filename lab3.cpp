ba515733e82bcfad098706d0c6f75fffcca99a04
Hadas Cohen <hc820@live.mdx.ac.uk>
2024-01-30 16:46:45 +0000

    second commit - changes made to code

--- lab3.cpp
+++ lab3.cpp
@@ -1,1 +1,23 @@
 //this is lab3
+#include <iostream>
+#include <string>
+
+int main(){
+    //Exercise 1
+    const int dayInWeek;
+    const int cmToInch;
+    const int currentMonth;
+    const string telephoneNumber;
+
+
+
+    //Exercise 2
+    //Find as many compile time error in the following program as you can.
+    int x, y;
+
+    std::cout << "Please enter two numbers: ";
+    std::cin >> x >> y;
+    std::cout << "The sum of " << x << "and" << y << "is: " << x + y << std::endl;
+
+    return;
+}
